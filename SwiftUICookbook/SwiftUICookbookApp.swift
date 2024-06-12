1d15d1f4951918664be473b82a79331450b2cc8d
Jaeseong Jeong <js104427@gmail.com>
2024-06-12 12:38:29 +0800

    Initial Commit

---
+++ SwiftUICookbook/SwiftUICookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICookbookApp.swift
+//  SwiftUICookbook
+//
+//  Created by Jaeseong Jeong on 12/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
