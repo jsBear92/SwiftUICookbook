fd90c440e5caf41547ace7ffb7f5c6806ee3a30c
Jaeseong Jeong <js104427@gmail.com>
2024-06-13 15:28:10 +0800

    changed file and module name

---
+++ SwiftUICookbook/StudyContents/Chapter3/LazyGrids.swift
@@ -0,0 +1,18 @@
+//
+//  DisplayingTabularContentWithLazyHGridAndLazyVGrid.swift
+//  SwiftUICookbook
+//
+//  Created by Jaeseong Jeong on 13/6/2024.
+//
+
+import SwiftUI
+
+struct DisplayingTabularContentWithLazyHGridAndLazyVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DisplayingTabularContentWithLazyHGridAndLazyVGrid()
+}
